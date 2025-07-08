24a16331f3e2b29f5c3141fa9640741109f61e44
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2025-07-08 12:31:13 +0200

    test

--- IntegrationService/src/IntegrationSimulator.IntegrationService.Program/Program.cs
+++ IntegrationService/src/IntegrationSimulator.IntegrationService.Program/Program.cs
@@ -9,7 +9,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
-
 var builder = Host.CreateApplicationBuilder(args);
 
 builder.Services.AddHttpClient<IJobAdClient, PlatsbankenClient>()
