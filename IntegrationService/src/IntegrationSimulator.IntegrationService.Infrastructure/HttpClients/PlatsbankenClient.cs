ef87739124c80a304b893fc25af1b6b41d2777a4
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2025-07-08 12:35:51 +0200

    test trigger

--- IntegrationService/src/IntegrationSimulator.IntegrationService.Infrastructure/HttpClients/PlatsbankenClient.cs
+++ IntegrationService/src/IntegrationSimulator.IntegrationService.Infrastructure/HttpClients/PlatsbankenClient.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net.Http.Json;
-using System.Text.Json;
 using IntegrationSimulator.IntegrationService.Constants;
 using IntegrationSimulator.IntegrationService.Domain.Interfaces;
 using IntegrationSimulator.IntegrationService.Domain.Models;
