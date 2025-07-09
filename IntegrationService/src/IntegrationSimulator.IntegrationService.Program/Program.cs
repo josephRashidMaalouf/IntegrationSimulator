c76bf412504e62af65982cc43bd9823e9eb94094
Joseph Rashid-Maalouf <joseph_maalouf@outlook.com>
2025-07-09 21:20:48 +0200

    .

--- IntegrationService/src/IntegrationSimulator.IntegrationService.Program/Program.cs
+++ IntegrationService/src/IntegrationSimulator.IntegrationService.Program/Program.cs
@@ -42,7 +42,7 @@ builder.Services.AddScoped<Consumer>();
 
 builder.Services.AddSingleton<IConnectionFactory, ConnectionFactory>(_ => new()
 {
-    Uri = new Uri(builder.Configuration["RabbitMQ:MqUri"] ?? string.Empty),
+    Uri = new Uri(builder.Configuration["RabbitMQ:Uri"] ?? string.Empty),
     ClientProvidedName = builder.Configuration["RabbitMQ:ClientProvidedName"] ?? string.Empty,
     HostName = builder.Configuration["RabbitMQ:HostName"] ?? string.Empty,
     UserName = builder.Configuration["RabbitMQ:Username"] ?? string.Empty,
